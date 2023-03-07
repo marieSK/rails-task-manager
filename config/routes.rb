f466a2c8a94450935090b20800ee560398d311c3
Maria Skrabalekova <mariaskrabalekova@gmail.com>
2023-03-07 14:20:16 +0100

    add CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # READ ALL
+  get 'tasks', to: 'tasks#index'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # READ ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
